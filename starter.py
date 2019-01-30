d2f70d6ab60dd1a8d6ab31cfc40bf8ee39ae8010
jweezy <jwest1@luc.edu>
2019-01-30 15:49:38 -0600

    added a "get" method to retrieve sepcific zettel

--- starter.py
+++ starter.py
@@ -4,3 +4,38 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/grab/<string:filename>')
+def grabZet(filename):
+    vals = parseFile(filename)
+    returnVal = '''
+{title}
+{summary}
+{url}
+{note}
+{tags}
+    '''.format(**vals)
+    return str(returnVal)
+
+
+def parseFile(file):
+    path = "/home/jweezy/Documents/Code/zg-tutorial/zettels/rheingold-examples/"
+    info = open(path+file, "r")
+    files = {file: ''}
+    dict = {"title":'', "summary": "", "url":'', "note":"", "tags":"" }
+    for i in info:
+        for j in dict.keys():
+            if j+":" in i:
+                currentTag = j
+                if j == "url":
+                    firstLine = i.split("url:")[1]
+                    break
+                firstLine = i.split(":")[1]
+                break
+        if firstLine != '':
+            dict.update({currentTag: firstLine})
+            firstLine = ''
+        else:
+            dict.update({currentTag: dict.get(currentTag)+ "\n" + i})
+    info.close()
+    return dict
